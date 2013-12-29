26626560ccafdcafd241db9259f6951e53283996
JN-Jones <jones_h1@web.de>
2013-12-29 14:35:51 +0100

    Fixed a few issues and added a new setting (version 1.5)

--- inc/languages/deutsch_du/dealbutton.lang.php
+++ inc/languages/deutsch_du/dealbutton.lang.php
@@ -1,6 +1,7 @@
 <?php
 $l['db_no_uid'] = "Fehler, keine UserID angegeben";
 $l['db_no_fid'] = "Fehler, keine ForenID angegeben";
+$l['db_dealt_with'] = "Fehler, dieser Deal wurde bereits abgeschloßen";
 $l['db_link'] = "Link zum Deal";
 $l['db_submit'] = "Deal bestätigt. Bitte informiere jetzt deinen Dealpartner per PN";
 $l['db_thread_error'] = "Fehler beim Erstellen des Themas.<br />Folgende Fehlermeldungen sind aufgetreten:<br />{1}<br />Bitte gehe wieder zurück";
--- inc/plugins/dealbutton.php
+++ inc/plugins/dealbutton.php
@@ -7,18 +7,19 @@ if(!defined("IN_MYBB"))
 $plugins->add_hook("postbit", "dealbutton_postbit");
 $plugins->add_hook("showthread_start", "dealbutton_create");
 $plugins->add_hook("datahandler_post_validate_post", "dealbutton_post_validate");
+$plugins->add_hook("admin_config_settings_begin", "dealbutton_update");
 
 function dealbutton_info()
 {
     return array(
-        "name"            => "Dealbutton",
-        "description"    => "Dealbutton in Handelsforen",
-        "website"        => "http://jonesboard.de/",
-        "author"        => "Jones",
-        "authorsite"    => "http://jonesboard.de/",
-        "version"        => "1.4",
-        "guid"             => "",
-        "compatibility" => "16*"
+        "name"			=> "Dealbutton",
+        "description"	=> "Dealbutton in Handelsforen",
+        "website"		=> "http://jonesboard.de/",
+        "author"		=> "Jones",
+        "authorsite"	=> "http://jonesboard.de/",
+        "version"		=> "1.5",
+        "guid"			=> "",
+        "compatibility"	=> "16*"
     );
 }
 
@@ -97,6 +98,17 @@ function dealbutton_install()
         "gid" => (int)$gid,
         );
     $db->insert_query("settings", $setting);
+
+    $setting = array(
+        "name" => "dealbutton_disable_double",
+        "title" => "Soll es möglich sein den Button mehr als einmal zu drücken?",
+        "description" => "",
+        "optionscode" => "yesno",
+        "value" => "no",
+        "disporder" => "7",
+        "gid" => (int)$gid,
+        );
+    $db->insert_query("settings", $setting);
     rebuild_settings();
 
     $template="
@@ -107,6 +119,8 @@ function dealbutton_install()
             "sid" => "-2",
             );
     $db->insert_query("templates", $templatearray);
+    
+   	$db->add_column("threads", "was_dealt", "boolean DEFAULT '0'");
 }
 
 function dealbutton_is_installed()
@@ -127,6 +141,37 @@ function dealbutton_uninstall()
     $db->delete_query("settings", "gid='".$g['gid']."'");
     rebuild_settings();
     $db->delete_query("templates", "title='postbit_dealbutton'");
+    $db->drop_column("threads", "was_dealt");
+}
+
+function dealbutton_update()
+{
+	global $mybb, $db;
+	if($mybb->input['action'] != "dbupdate")
+	    return;
+
+	if(!$db->field_exists("was_dealt", "threads")) {
+		//Update vor 1.5
+	   	$db->add_column("threads", "was_dealt", "boolean DEFAULT '0'");
+		
+	    $query = $db->simple_select("settinggroups", "gid", "name='dealbutton'");
+	    $gid = $db->fetch_field($query, "gid");
+	    $setting = array(
+	        "name" => "dealbutton_disable_double",
+	        "title" => "Soll es möglich sein den Button mehr als einmal zu drücken?",
+	        "description" => "",
+	        "optionscode" => "yesno",
+	        "value" => "no",
+	        "disporder" => "7",
+	        "gid" => (int)$gid,
+	        );
+	    $db->insert_query("settings", $setting);
+	    rebuild_settings();
+	    echo "Erfolgreich auf Version 1.5 geupdatet<br />";
+	}
+	
+	echo "Updates komplett abgeschlossen";
+	exit();
 }
 
 function dealbutton_activate()
@@ -148,11 +193,14 @@ function dealbutton_postbit($post)
 //    echo "<pre>";var_dump($post);echo "</pre>";
     global $templates, $theme, $mybb, $db;
     $fids = explode(',', $mybb->settings['dealbutton_activate_fid']);
-    if(in_array($post['fid'], $fids)||(sizeOf($fids)==1&&$fids[0]==0)) {
-        if($post['replyto']!=0) {
-            $query = $db->simple_select("posts", "uid", "pid='{$post['replyto']}'");
-            $uid = $db->fetch_field($query, "uid");
-            if($uid==$mybb->user['uid']&&$uid!=$post['uid'])
+    if(in_array($post['fid'], $fids) || (sizeOf($fids) == 1 && $fids[0] == 0)) {
+        if($post['replyto'] != 0) {
+            $query = $db->simple_select("threads", "uid, was_dealt", "firstpost='{$post['replyto']}'");
+            $ar = $db->fetch_array($query);
+           	$uid = $ar['uid'];
+            if($ar['was_dealt'] && $mybb->settings['dealbutton_disable_double'] != 1)
+                return;
+            if($uid == $mybb->user['uid'] && $uid != $post['uid'])
                 eval("\$post['button_dealbutton'] = \"".$templates->get("postbit_dealbutton")."\";");
         }
     }
@@ -163,18 +211,22 @@ function dealbutton_create()
 {
     global $db, $mybb, $lang;
     $lang->load("dealbutton");
-    if($mybb->input['action']=="dealbutton") {
+    if($mybb->input['action'] == "dealbutton") {
 		$link_plain = $mybb->settings['bburl']."/".get_post_link($mybb->input['pid'],$mybb->input['tid'])."#pid{$mybb->input['pid']}";
-        echo $link_plain;
-	    if(!$mybb->input['uid'])
+
+   	    if(!$mybb->input['uid'])
             redirect($link_plain, $lang->db_no_uid);
 
-        if(!$mybb->settings['dealbutton_fid']||$mybb->settings['dealbutton_fid']==0)
+        if(!$mybb->settings['dealbutton_fid'] || $mybb->settings['dealbutton_fid']==0)
             redirect($link_plain, $lang->db_no_fid);
 
-        $name = $db->fetch_field($db->simple_select("users", "username", "uid='{$mybb->input['uid']}'"), "username");
-        $aname = $db->fetch_field($db->simple_select("users", "username", "uid='{$mybb->input['auid']}'"), "username");
-        $osubject = $db->fetch_field($db->simple_select("posts", "subject", "pid='{$mybb->input['pid']}'"), "subject");
+		$dealt_with = $db->fetch_field($db->simple_select("threads", "was_dealt", "tid='".(int)$mybb->input['tid']."'"), "was_dealt");
+		if($dealt_with && $mybb->settings['dealbutton_disable_double'] != 1)
+		    redirect($link_plain, $lang->db_dealt_with);
+
+        $name = $db->fetch_field($db->simple_select("users", "username", "uid='".(int)$mybb->input['uid']."'"), "username");
+        $aname = $db->fetch_field($db->simple_select("users", "username", "uid='".(int)$mybb->input['auid']."'"), "username");
+        $osubject = $db->fetch_field($db->simple_select("posts", "subject", "pid='".(int)$mybb->input['pid']."'"), "subject");
         $subject = str_replace('{creator}', $name, $mybb->settings['dealbutton_text_subject']);
         $subject = str_replace('{answer}', $aname, $subject);
         $subject = str_replace('{subject}', $osubject, $subject);
@@ -238,6 +290,9 @@ function dealbutton_create()
 		}
         $answer->insert_post();
         $link_plain = get_post_link($pid,$tid)."#pid{$pid}";
+        
+        $db->update_query("threads", array("was_dealt" => 1), "tid='".(int)$mybb->input['tid']."'");
+        
         redirect($link_plain, $lang->db_submit);
     }
 }
@@ -249,7 +304,7 @@ function dealbutton_post_validate($handler)
 	    return true;
 	$errors = &$handler->errors;
 	$post = &$handler->data;
-	if(!array_key_exists("is_dealbutton", $post)||!$post["is_dealbutton"])
+	if(!array_key_exists("is_dealbutton", $post) || !$post["is_dealbutton"])
 	    return true;
 	if(array_key_exists("post_flooding", $errors))
 	    unset($errors['post_flooding']);
